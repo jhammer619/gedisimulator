dc31143c6e5b8afe35121e31cb51aec4dfc9d982
StevenHancock <devnull@localhost>
2017-09-18 18:35:26 -0400

    Program to geolocate LVIS and ALS started.

---
+++ lvisBullseye.c
@@ -0,0 +1,71 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+#include "stdint.h"
+#include "hdf5.h"
+#include "tools.h"
+#include "tools.c"
+#include "libLasRead.h"
+#include "libLasProcess.h"
+#include "libLidarHDF.h"
+#include "gediIO.h"
+
+
+/*#############################*/
+/*# Uses simulated waveforms #*/
+/*# to correlte to LVIS to   #*/
+/*# coregister     2017      #*/
+/*# svenhancock@gmail.com    #*/
+/*############################*/
+
+/*#######################################*/
+/*# Copyright 2015-2017, Steven Hancock #*/
+/*# The program is distributed under    #*/
+/*# the terms of the GNU General Public #*/
+/*# License.    svenhancock@gmail.com   #*/
+/*#######################################*/
+
+
+/*########################################################################*/
+/*# This file is part of the NASA GEDI simulator, gediRat.               #*/
+/*#                                                                      #*/
+/*# gediRat is free software: you can redistribute it and/or modify      #*/
+/*# it under the terms of the GNU General Public License as published by #*/
+/*# the Free Software Foundation, either version 3 of the License, or    #*/
+/*#  (at your option) any later version.                                 #*/
+/*#                                                                      #*/
+/*# gediRat is distributed in the hope that it will be useful,           #*/
+/*# but WITHOUT ANY WARRANTY; without even the implied warranty of       #*/
+/*#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       #*/
+/*#   GNU General Public License for more details.                       #*/
+/*#                                                                      #*/
+/*#    You should have received a copy of the GNU General Public License #*/
+/*#    along with gediRat.  If not, see <http://www.gnu.org/licenses/>.  #*/
+/*########################################################################*/
+
+
+
+/*####################################*/
+/*control structure*/
+
+typedef struct{
+  /*input/output*/
+  gediIOstruct gediIO; /*input/output structure*/
+
+
+}control;
+
+
+/*########################################################################*/
+/*main*/
+
+int main(int argc,char **argv)
+{
+
+  return(0);
+}/*main*/
+
+/*the end*/
+/*########################################################################*/
+
