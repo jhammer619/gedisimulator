27b580f13351a10866addd142e9a269ac50d90f9
Steven Hancock <svenhancock@gmail.com>
2017-11-16 18:10:05 -0500

    Octree levels turned off to bypass current bug.

--- gediRat.c
+++ gediRat.c
@@ -723,8 +723,8 @@ control *readCommands(int argc,char **argv)
 
   /*octree*/
   dimage->gediRat.useOctree=1;
-  dimage->gediRat.octLevels=4;
-  dimage->gediRat.nOctTop=20;   
+  dimage->gediRat.octLevels=0;
+  dimage->gediRat.nOctTop=40;   
   dimage->gediRat.octree=NULL;
 
   /*gridding options*/
