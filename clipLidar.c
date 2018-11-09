37f43ea9d1904c23300fbaafa769ec49a03f1c34
Steven Hancock <svenhancock@gmail.com>
2018-11-09 13:16:17 +0000

    New program to clip lidar data started, Not finished.

---
+++ clipLidar.c
@@ -0,0 +1,71 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+#include "stdint.h"
+#include "tools.h"
+#include "tools.c"
+#include "hdf5.h"
+#include "libLasRead.h"
+#include "libLidVoxel.h"
+#include "libLasProcess.h"
+#include "libLidarHDF.h"
+#include "libOctree.h"
+#include "gediIO.h"
+
+
+/*#########################*/
+/*# Clips out areas of    #*/
+/*# lidar daya    2018    #*/
+/*# svenhancock@gmail.com #*/
+/*#########################*/
+
+/*#######################################*/
+/*# Copyright 2015-2018, Steven Hancock #*/
+/*# The program is distributed under    #*/
+/*# the terms of the GNU General Public #*/
+/*# License.    svenhancock@gmail.com   #*/
+/*#######################################*/
+
+/*########################################################################*/
+/*# This file is part of the NASA GEDI simulator, gediRat.               #*/
+/*#                                                                      #*/
+/*# gediRat is free software: you can redistribute it and/or modify      #*/
+/*# it under the terms of the GNU General Public License as published by #*/
+/*# the Free Software Foundation, either version 3 of the License, or    #*/
+/*#  (at your option) any later version.                                 #*/
+/*#                                                                      #*/
+/*# gediRat is distributed in the hope that it will be useful,           #*/
+/*# but WITHOUT ANY WARRANTY; without even the implied warranty of       #*/
+/*#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       #*/
+/*#   GNU General Public License for more details.                       #*/
+/*#                                                                      #*/
+/*#    You should have received a copy of the GNU General Public License #*/
+/*#    along with gediRat.  If not, see <http://www.gnu.org/licenses/>.  #*/
+/*########################################################################*/
+
+
+
+
+/*####################################*/
+/*control structure*/
+
+typedef struct{
+
+}control;
+
+
+
+
+/*####################################*/
+/*main*/
+
+int main(int argc,char **argv)
+{
+
+  return(0);
+}/*main*/
+
+/*the end*/
+/*####################################*/
+
