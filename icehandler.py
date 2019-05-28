181265fd7f3e385a9a1298671855ee08d1025e56
Steven Hancock <svenhancock@gmail.com>
2019-05-28 17:18:20 +0100

    ICESat-2 simulation handler finished.

--- icehandler.py
+++ icehandler.py
@@ -44,7 +44,7 @@ class iceSim(object):
     self.iterationN=temp[14]
     self.refdem=temp[15]
     self.noiseInt=temp[16]
-    self.signal=temp[17]
+    self.signal=np.array(temp[17],dtype=np.int16)
     self.epsg=epsg
     return
 
@@ -54,10 +54,13 @@ class iceSim(object):
     '''Write the output HDF5 file'''
     # convert some parameters
     numb=self.x.shape[0]
-    delta_time=
+    self.setDists()
+    delta_time=self.dists*0.7/7599.68
     inProj=Proj(init="epsg:"+str(self.epsg))
     outProj=Proj(init="epsg:"+str(4326))
-    lon,lat=ransform(inProj,outProj,self.x,self.y)
+    lon,lat=transform(inProj,outProj,self.x,self.y)
+    segment_dist_x=np.remainder(np.array(self.dists/20))
+    segment_id=np.around(np.array(self.dists/20))
     # open output
     f=h5py.File(outNamen,'w')
     # make top level groups
@@ -70,21 +73,40 @@ class iceSim(object):
     f['GT1L'].create_group('veg_truth')
     # populate data
     f['#ref#'].create_dataset('a',data=[0,0])
+    f['GT1L']['geolocation'].create_dataset('segment_dist_x',data=segment_dist_x)
+    f['GT1L']['geolocation'].create_dataset('segment_id',data=segment_id)
     f['GT1L']['geolocation'].create_dataset('sigma_h',data=np.full(numb,0.4))
     f['GT1L']['geolocation'].create_dataset('surf_type',data=np.full(numb,1))
+    f['GT1L']['geolocation'].create_dataset('segment_length',data=np.full(numb,20))
+    f['GT1L']['heights'].create_dataset('delta_time',data=delta_time)
     f['GT1L']['heights'].create_dataset('h_ph',data=self.z)
     f['GT1L']['heights'].create_dataset('lon_ph',data=lon)
     f['GT1L']['heights'].create_dataset('lat_ph',data=lat)
+    f['GT1L']['heights'].create_dataset('signal_conf_photon',data=self.signal)
+    f['GT1L']['orbit_info'].create_dataset('rgt',data=(1))
     f['GT1L']['veg_truth'].create_dataset('x_utm',data=self.x)
     f['GT1L']['veg_truth'].create_dataset('y_utm',data=self.y)
-
-
-
-
+    f['GT1L']['veg_truth'].create_dataset('refDEM',data=self.refdem)
+    f['GT1L']['veg_truth'].create_dataset('rh50',data=self.RH50)
+    f['GT1L']['veg_truth'].create_dataset('rh60',data=self.RH60)
+    f['GT1L']['veg_truth'].create_dataset('rh75',data=self.RH75)
+    f['GT1L']['veg_truth'].create_dataset('rh90',data=self.RH90)
+    f['GT1L']['veg_truth'].create_dataset('rh95',data=self.RH95)
+    f['GT1L']['veg_truth'].create_dataset('canopyz',data=self.CanopyZ)
+    f['GT1L']['veg_truth'].create_dataset('canopy_cover',data=self.canopycover)
     # close up
     f.close()
     print("Written to",outNamen)
     return
+
+
+  #################################
+
+  def setDists(self):
+    '''Set distances'''
+    self.minX=np.min(self.x)
+    self.minY=np.min(self.y)
+    self.dists=np.sqrt((self.x-self.minX)**2+(self.y-self.minY)**2)
 
 # iceSim() class end
 ########################################
